4876d7d6ff2680f44e9e447233b699f10120b9ba
alzuhairk <alzuhair@uw.edu>
2016-10-04 18:51:10 -0700

    Add files via upload

--- decoder3_8.sv
+++ decoder3_8.sv
@@ -1,16 +1,17 @@
-module decoder3_8(in, out)
+module decoder3_8(in, enable, out)
 input logic [2:0] in; 
+input logic enable;
 output logic [7:0] out; 
 
-out[0] = ~in[0] & ~in[1] & ~in[2];
-out[1] = in[0] & ~in[1] & ~in[2];
-out[2] = ~in[0] & in[1] & ~in[2];
-out[3] = in[0] & in[1] & ~in[2];
+out[0] = ~in[0] & ~in[1] & ~in[2] & enable ;
+out[1] = in[0] & ~in[1] & ~in[2] & enable;
+out[2] = ~in[0] & in[1] & ~in[2] & enable;
+out[3] = in[0] & in[1] & ~in[2] & enable;
 
-out[4] = ~in[0] & ~in[1] & in[2];
-out[5] = in[0] & ~in[1] & in[2];
-out[6] = ~in[0] & in[1] & in[2];
-out[7] = in[0] & in[1] & in[2];
+out[4] = ~in[0] & ~in[1] & in[2] & enable;
+out[5] = in[0] & ~in[1] & in[2] & enable;
+out[6] = ~in[0] & in[1] & in[2] & enable;
+out[7] = in[0] & in[1] & in[2] & enable;
 
 
 
