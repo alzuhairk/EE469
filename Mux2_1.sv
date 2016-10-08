cde42def00a9f9129f5a2292fcfd5526484a5d2a
sleisle <sleisle@gmail.com>
2016-10-07 20:00:04 -0700

    Add files via upload

--- Mux2_1.sv
+++ Mux2_1.sv
@@ -1,10 +1,41 @@
-module Mux2_1 (a, b, x, out)
+module Mux2_1 (a, b, x, out);
 	input logic a, b, x;
 	output logic out;
 	
-	out = (a & ~x) | (b & x);
+	assign out = (a & ~x) | (b & x);
 
 
-endmodlue
+endmodule
 
 // Testbench
+
+module mux2_1Testbench();
+	parameter ClockDelay = 5000;
+
+	logic a, b, x, out;
+	logic clk;
+	
+	Mux2_1 dut (.a, .b, .x, .out);
+	
+	// Force %t's to print in a nice format.
+	initial $timeformat(-9, 2, " ns", 10);
+
+	initial begin // Set up the clock
+		clk <= 0;
+		forever #(ClockDelay/2) clk <= ~clk;
+	end
+	
+	initial begin
+		a <= 1;
+		b <= 0;
+		x <= 0;			@(posedge clk);
+							@(posedge clk);
+		x <= 1;			@(posedge clk);
+							@(posedge clk);
+		x <= 0;			@(posedge clk);
+							@(posedge clk);
+			
+		$stop;
+	end
+
+endmodule
--- Mux32_1.sv
+++ Mux32_1.sv
@@ -1,7 +1,8 @@
-module Mux32_1 (in[31:0], x[4:0], out)
+module Mux32_1 (in, x, out);
 	input logic[31:0] in;
 	input logic[4:0] x;
 	output logic out;
+	logic[1:0] temp;
 	
 	Mux16_1 m1 (.in(in[31:16]), .x(x[3:0]), .out(temp[1]));
 	Mux16_1 m2 (.in(in[15:0]), .x(x[3:0]), .out(temp[0]));
@@ -9,7 +10,7 @@ module Mux32_1 (in[31:0], x[4:0], out)
 
 endmodule
 
-module Mux16_1 (in[15:0], x[3:0], out)
+module Mux16_1 (in, x, out);
 	input logic[15:0] in;
 	input logic[3:0] x;
 	output logic out;
@@ -18,9 +19,10 @@ module Mux16_1 (in[15:0], x[3:0], out)
 	Mux8_1 m1 (.in(in[15:8]), .x(x[2:0]), .out(temp[1]));
 	Mux8_1 m2 (.in(in[7:0]), .x(x[2:0]), .out(temp[0]));
 	Mux2_1 m3 (.a(temp[1]), .b(temp[0]), .x(x[3]), .out(out));
+	
 endmodule
 
-module Mux8_1 (in[7:0], x[2:0], out)
+module Mux8_1 (in, x, out);
 	input logic[7:0] in;
 	input logic[2:0] x;
 	output logic out;
@@ -32,7 +34,7 @@ module Mux8_1 (in[7:0], x[2:0], out)
 
 endmodule
 
-module Mux4_1 (in[3:0], x[1:0], out)
+module Mux4_1 (in, x, out);
 	input logic[3:0] in;
 	input logic[1:0] x;
 	output logic out;
@@ -41,5 +43,40 @@ module Mux4_1 (in[3:0], x[1:0], out)
 	Mux2_1 m1 (.a(in[3]), .b(in[2]), .x(x[0]), .out(temp[1]));
 	Mux2_1 m2 (.a(in[1]), .b(in[0]), .x(x[0]), .out(temp[0]));
 	Mux2_1 m3 (.a(temp[1]), .b(temp[0]), .x(x[1]), .out(out));
+
+endmodule
+
+// Testbench
+
+module mux32_1Testbench();
+	parameter ClockDelay = 5000;
+
+	logic[31:0] in;
+	logic[4:0] x;
+	logic out;
+	logic clk;
+	
+	Mux32_1 dut (.in, .x, .out);
+	
+	// Force %t's to print in a nice format.
+	initial $timeformat(-9, 2, " ns", 10);
+
+	initial begin // Set up the clock
+		clk <= 0;
+		forever #(ClockDelay/2) clk <= ~clk;
+	end
+	
+	initial begin
+		in <= 32'b00001111000011110000111100001111; // 00000 goes to 0 after b
+		x <= 5'b00000;			@(posedge clk);
+									@(posedge clk);
+		x <= 5'b00100; 		@(posedge clk);
+									@(posedge clk);
+		x <= 5'b01000; 		@(posedge clk);
+									@(posedge clk);
+									@(posedge clk);
+			
+		$stop;
+	end
 
 endmodule
