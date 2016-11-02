9beadeb120d3b7415b7bf7deca3a1d47516f0327
sleisle <sleisle@gmail.com>
2016-11-02 14:50:01 -0700

    Add files via upload

---
+++ mux64x2_1.sv
@@ -0,0 +1,14 @@
+module mux64x2_1 (zero, one, control, out)
+	input logic [63:0] zero, one;
+	input logic control;
+	output logic [63:0] out;
+	
+	genvar i;
+	generate
+		for (i = 0; i < 64; i++) begin: generateMux
+			mux2_1 m (.a(one[i]), .b(zero[i]), .x(control), .out(out[i]));
+		end
+	endgenerate
+
+
+endmodule
