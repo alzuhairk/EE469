8a1bd039f4ea7ad079ebd2bd49b6760c81a26f0e
sleisle <sleisle@gmail.com>
2016-11-17 17:30:01 -0800

    Add files via upload

--- cpu.sv
+++ cpu.sv
@@ -29,7 +29,6 @@ module cpu (clk, reset);
 	assign Imm19 = instruction[23:5];
 	assign Imm26 = instruction[25:0];
 	assign opcode = instruction[31:21];
-	assign Rd = instruction[4:0];
 	assign Rn = instruction[9:5];
 	assign Rm = instruction[20:16];
 	assign DT_Address9 = instruction[20:12];
@@ -46,66 +45,77 @@ module cpu (clk, reset);
 				flags = 10'b???001100?;
 				ALUOp = 3'b???;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b100101?????: begin // BL
 				flags = 10'b???101110?;
 				ALUOp = 3'b???;
 				dataMemReadEn = 1'b0;
+				Rd = 5'b11110;
 			end
 			
 			11'b01010100???: begin // B.LT
 				flags = {5'b???00, BLTTaken, 4'b0?0?};
 				ALUOp = 3'b???;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b10110100???: begin // CBZ
 				flags = {5'b00?00, aluZero, 4'b0?0?};
 				ALUOp = 3'b000;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b10101011000: begin // ADDS
 				flags = 10'b100100?00?;
 				ALUOp = 3'b010;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b11101011000: begin // SUBS
 				flags = 10'b100100?00?;
 				ALUOp = 3'b011;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b11010110000: begin // BR
 				flags = 10'b0??00???1?;
 				ALUOp = 3'b???;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b1001000100?: begin // ADDI
 				flags = 10'b?10100?001;
 				ALUOp = 3'b010;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 			
 			11'b11111000010: begin // LDUR
 				flags = 10'b?11100?000;
 				ALUOp = 3'b010;
 				dataMemReadEn = 1'b1;
+				Rd = instruction[4:0];
 			end
 			
 			11'b11111000000: begin // STUR
 				flags = 10'b01?010??00;
 				ALUOp = 3'b010;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end			
 			
 			default: begin
 				flags = {3'b?, 1'b0, 1'b0, 5'b?};
 				ALUOp = 3'b???;
 				dataMemReadEn = 1'b0;
+				Rd = instruction[4:0];
 			end
 		endcase
 	end
