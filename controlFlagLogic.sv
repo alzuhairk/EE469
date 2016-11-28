78645714d1e2cabe67fc24710d4124947b61a567
sleisle <sleisle@gmail.com>
2016-11-27 18:58:26 -0800

    Add files via upload

--- controlFlagLogic.sv
+++ controlFlagLogic.sv
@@ -1,10 +1,15 @@
-module controlFlagLogic (opcode, instruction, flags, ALUOp, dataMemReadEn, Rd);
+`timescale 1 ps/1 ps
+
+module controlFlagLogic (opcode, instruction, aluNegativePrev, aluOverflowPrev, flags, ALUOp, dataMemReadEn, Rd);
 	input logic [10:0] opcode;
 	input logic [31:0] instruction;
+	input logic aluNegativePrev, aluOverflowPrev;
 	output logic [9:0] flags;
 	output logic [2:0] ALUOp;
 	output logic dataMemReadEn;
 	output logic [4:0] Rd;
+	
+	xor #50 blt (BLTTaken, aluNegativePrev, aluOverflowPrev);
 
 	always_comb begin
 		casez (opcode)
@@ -30,7 +35,7 @@ module controlFlagLogic (opcode, instruction, flags, ALUOp, dataMemReadEn, Rd);
 			end
 			
 			11'b10110100???: begin // CBZ
-				flags = {5'b00?00, aluZero, 4'b0?0?};
+				flags = {5'b00?00, /*SET TO USE ACCEL BRANCHES */ 1'b0, 4'b0?0?};
 				ALUOp = 3'b000;
 				dataMemReadEn = 1'b0;
 				Rd = instruction[4:0];
--- cpu.sv
+++ cpu.sv
@@ -29,17 +29,17 @@ module cpu (clk, reset);
 	// PIPELINE LOGICS //
 	logic [31:0] ifInstruction, rfInstruction, exInstruction, dmInstruction, wbInstruction;
 	logic [13:0] exFlags, dmFlags, wbFlags;
+	logic [4:0] wbRd;
+	logic [63:0] rfDa, rfDb, exAluOutput, dmMemToRegToL;
+	
 	assign Imm19 = instruction[23:5];
 	assign Imm26 = instruction[25:0];
 	assign opcode = instruction[31:21];
-	assign Rn = instruction[9:5];
 	assign Rm = instruction[20:16];
-	assign DT_Address9 = instruction[20:12];
 	assign CondAddr19 = instruction[23:5];
-	assign ALU_Imm12 = instruction[21:10];
 	
 	// B.LT XOR
-	xor #50 blt (BLTTaken, aluNegativePrev, aluOverflowPrev);
+	
 	
 	// Flags = reg2loc,ALUSrc,MemToReg,RegWrite,MemWrite,BrTaken,UnCondBr,LBranch,IsBr,AddImm;
 	// rfFlags = aluop, dmreadenable, reg2loc,ALUSrc,MemToReg,RegWrite,MemWrite,BrTaken,UnCondBr,LBranch,IsBr,AddImm;
@@ -65,48 +65,62 @@ module cpu (clk, reset);
 	
 	// REGISTER FETCH //
 	
-	regfile registerFile (.ReadData1(readData1), .ReadData2(readData2), .WriteData(BLMuxOut), .ReadRegister1(Rn), .ReadRegister2(reg2LocMuxOut), .WriteRegister(Rd), .RegWrite(flags[6]), .clk(clk));
+	regfile registerFile (.ReadData1(readData1), .ReadData2(readData2), .WriteData(BLMuxOut), .ReadRegister1(rfInstruction[9:5]), .ReadRegister2(reg2LocMuxOut), .WriteRegister(wbRd), .RegWrite(wbFlags[6]), .clk(clk));
 	
-	register32Bit rfInstructionRegister (.writeEnable(1'b1), .writeData(ifInstruction), .dataOut(rfInstruction), .reset(reset), .clk(clk));
+	register32Bit rfInstructionRegister (.writeEnable(1'b1), .writeData(instruction), .dataOut(rfInstruction), .reset(reset), .clk(clk));
+	register64Bit rfDaRegister (.writeEnable(1'b1), .writeData(readData1), .dataOut(rfDa), .reset(reset), .clk(clk));
+	register64Bit rfDbRegister (.writeEnable(1'b1), .writeData(ALUSrcMuxOutput), .dataOut(rfDb), .reset(reset), .clk(clk));
 	
-	controlFlagLogic controlLogic (.opcode(opcode), .instruction(instruction), .flags(flags), .ALUOp(ALUOp), .dataMemReadEn(dataMemReadEn), .Rd(Rd));
+	controlFlagLogic controlLogic (.opcode(opcode), .instruction(instruction), .aluNegativePrev(aluNegativePrev), .aluOverflowPrev(aluOverflowPrev), .flags(flags), .ALUOp(ALUOp), .dataMemReadEn(dataMemReadEn), .Rd(Rd));
 	
-	signExtend9 se9 (.in(DT_Address9), .out(DAddr9SEOut));
-	zeroExtend ze (.in(ALU_Imm12), .out(ALU_Imm12ZEOut));
+	signExtend9 se9 (.in(rfInstruction[20:12]), .out(DAddr9SEOut));
+	zeroExtend ze (.in(rfInstruction[21:10]), .out(ALU_Imm12ZEOut));
 	
 	mux64x2_1 ALUSrcMux (.zero(readData2), .one(ADDImmMuxOut), .control(flags[8]), .out(ALUSrcMuxOutput));
-	mux64x2_1 BLMux (.zero(MemToRegMuxOut), .one(adderToMux0), .control(flags[2]), .out(BLMuxOut));
 	mux64x2_1 ADDImmMux (.zero(DAddr9SEOut), .one(ALU_Imm12ZEOut), .control(flags[0]), .out(ADDImmMuxOut));
 	
 	genvar i;
 	generate
-		for (i = 0; i < 5; i++) begin: regMux
-			mux2_1 m (.a(Rm[i]), .b(Rd[i]), .x(flags[9]), .out(reg2LocMuxOut[i]));
+		for (i = 0; i < 5; i++) begin: regMux // Reg2LocMux
+			mux2_1 m (.a(rfInstruction[i + 16]), .b(rfInstruction[i]), .x(flags[9]), .out(reg2LocMuxOut[i]));
 		end
 	
 	endgenerate	
 	
 	// EXECUTE //
 	
-	alu arithmeticLogic (.A(readData1), .B(ALUSrcMuxOutput), .cntrl(ALUOp), .result(aluOutput), .negative(aluNegative), .zero(aluZero), .overflow(aluOverflow), .carry_out(aluCarryOut));
+	alu arithmeticLogic (.A(rfDa), .B(rfDb), .cntrl(exFlags[13:11]), .result(aluOutput), .negative(aluNegative), .zero(aluZero), .overflow(aluOverflow), .carry_out(aluCarryOut));
 	D_FF aluNegSave (.q(aluNegativePrev), .d(aluNegative), .reset(reset), .clk(clk));
 	D_FF aluOvSave (.q(aluOverflowPrev), .d(aluOverflow), .reset(reset), .clk(clk));
 	
 	register32Bit exInstructionRegister (.writeEnable(1'b1), .writeData(rfInstruction), .dataOut(exInstruction), .reset(reset), .clk(clk));
-	register14Bit exFlags ((.writeEnable(1'b1), .writeData({ALUOp, dataMemReadEn, flags}), .dataOut(exFlags), .reset(reset), .clk(clk)));
+	register14Bit exFlagsRegister (.writeEnable(1'b1), .writeData({ALUOp, dataMemReadEn, flags}), .dataOut(exFlags), .reset(reset), .clk(clk));
+	register64Bit exAluOutRegister (.writeEnable(1'b1), .writeData(aluOutput), .dataOut(exAluOutput), .reset(reset), .clk(clk));
+	
 	// DATA MEMORY //
 	
-	datamem dataMemory (.address(aluOutput), .write_enable(flags[5]), .read_enable(dataMemReadEn), .write_data(readData2), .clk(clk), .xfer_size(4'b1000), .read_data(readDataMem));
-	fastAdder FA0 (.A(address), .B(64'h0000000000000004), .cntrl(1'b0), .result(adderToMux0), .cOut(cOut0), .overflow(overflow0)); // control is 0 to do addition
-	fastAdder FA1 (.A(shiftLeftToAdder), .B(address), .cntrl(1'b0), .result(adderToMux1), .cOut(cOut1), .overflow(overflow1)); // control is 0 to do addition
-
+	datamem dataMemory (.address(exAluOutput), .write_enable(dmFlags[5]), .read_enable(dmFlags[10]), .write_data(readData2), .clk(clk), .xfer_size(4'b1000), .read_data(readDataMem));
+	
 	register32Bit dmInstructionRegister (.writeEnable(1'b1), .writeData(exInstruction), .dataOut(dmInstruction), .reset(reset), .clk(clk));
-	register14Bit dmFlags ((.writeEnable(1'b1), .writeData(exFlags), .dataOut(dmFlags), .reset(reset), .clk(clk)));
-	mux64x2_1 MemToRegMux (.zero(aluOutput), .one(readDataMem), .control(flags[7]), .out(MemToRegMuxOut));
+	register14Bit dmFlagsRegister (.writeEnable(1'b1), .writeData(exFlags), .dataOut(dmFlags), .reset(reset), .clk(clk));
+	register64Bit dmMemToRegToLRegister (.writeEnable(1'b1), .writeData(MemToRegMuxOut), .dataOut(dmMemToRegToL), .reset(reset), .clk(clk));
+	
+	mux64x2_1 MemToRegMux (.zero(exAluOutput), .one(readDataMem), .control(dmFlags[7]), .out(MemToRegMuxOut));
 	
 	// WRITE BACK //
 	register32Bit wbInstructionRegister (.writeEnable(1'b1), .writeData(dmInstruction), .dataOut(wbInstruction), .reset(reset), .clk(clk));
-	register14Bit dmFlags ((.writeEnable(1'b1), .writeData(dmFlags), .dataOut(wbFlags), .reset(reset), .clk(clk)));
+	register14Bit wbFlagsRegister (.writeEnable(1'b1), .writeData(dmFlags), .dataOut(wbFlags), .reset(reset), .clk(clk));
+	
+	mux64x2_1 BLMux (.zero(dmMemToRegToL), .one(adderToMux0), .control(wbFlags[2]), .out(BLMuxOut));
+	
+	generate
+		for (i = 1; i < 5; i++) begin: setRd // rd = 30 if BL
+			mux2_1 m (.a(1'b1), .b(wbInstruction[i]), .x(wbFlags[2]), .out(wbRd[i]));
+		end
+		mux2_1 m (.a(1'b0), .b(wbInstruction[0]), .x(wbFlags[2]), .out(wbRd[0]));
+	
+	endgenerate	
+	
 endmodule
 
 module cpuTestbench ();
